c8cb5b2dfadd785ac543e254495ac9506e3d5e54
kytomin <77231491+kython-c@users.noreply.github.com>
2023-03-22 20:25:10 +0300

    fix bug with search view

--- app/src/main/java/com/meowplex/text_editor_app/extensions/show_toast_and_require_permissions.kt
+++ app/src/main/java/com/meowplex/text_editor_app/extensions/show_toast_and_require_permissions.kt
@@ -3,9 +3,9 @@ package com.meowplex.text_editor_app.extensions
 import android.content.Context
 import android.widget.Toast
 import com.meowplex.text_editor_app.R
-import com.meowplex.text_editor_app.repository.PermissionRepository
+import com.meowplex.text_editor_app.utils.PermissionManager
 
 fun Context.showToastAndRequirePermissions() {
-    PermissionRepository().requireStoragePermission()
+    PermissionManager().requireStoragePermission()
     Toast.makeText(this, getString(R.string.give_permissions), Toast.LENGTH_SHORT).show()
 }
--- app/src/main/java/com/meowplex/text_editor_app/utils/SearchManager.kt
+++ app/src/main/java/com/meowplex/text_editor_app/utils/SearchManager.kt
@@ -2,16 +2,33 @@ package com.meowplex.text_editor_app.utils
 
 import com.meowplex.text_editor_app.model.FileModel
 
-class SearchManager(private val files: List<FileModel>) {
+class SearchManager(files: List<FileModel>) {
+
+    private var files: List<FileModel> = listOf()
+    private var lastQuery: String = ""
+
+    init {
+        setFiles(files)
+    }
+    fun setFiles(files: List<FileModel>){
+        this.files = files.sortedByDescending { f -> f.lastOpeningDate }
+    }
+
+    fun getFiles(): List<FileModel> = this.files
 
     fun search(query: String): List<FileModel> {
+        lastQuery = query
         val response = mutableListOf<FileModel>()
-        for (file in files){
-            if ((file.fileName + file.path).contains(query)){
+        for (file in files) {
+            if (file.fileName.contains(query)) {
                 response.add(file)
             }
         }
         return response
+    }
+
+    fun research(): List<FileModel>{
+        return search(lastQuery)
     }
 
 }
